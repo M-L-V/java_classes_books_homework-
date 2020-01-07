b1a0f680b761a22023f872d96adda5b9b61c2b0a
Morgaine <morgainedv@gmail.com>
2020-01-07 17:04:11 +0000

    adds stock count method to library class

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,2 +1,7 @@
 public class Book {
+
+    private String title;
+    private String genre;
+    private String author;
+
 }
--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -4,6 +4,11 @@ public class Library {
 
     private ArrayList<Book> stock;
 
+    public Library(){
+        this.stock = new ArrayList<Book>();
+    }
 
-
+    public int stockCount() {
+        return stock.size();
+    }
 }
